1b8a21bbfac200f9839e3dd72639b0d8b5b31939
sankar-coder <92115760+sankar-coder@users.noreply.github.com>
2022-10-05 21:55:54 +0530

    Create ransom-note.java

---
+++ ransom-note.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+        for(char ch: magazine.toCharArray()) {
+            count[ch - 'a']++;
+        }
+        for(char ch: ransomNote.toCharArray()) {
+            int val = ch - 'a';
+            if(count[val] <= 0) return false;
+            count[val]--;
+        }
+        return true;
+    }
+}
